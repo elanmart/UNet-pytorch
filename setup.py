59d433e71761305fa3af9713af4cb83ffa131388
Marcin Elantkowski <marcin.elantkowski@gmail.com>
2017-06-03 17:06:50 +0200

    update ignore

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup, find_packages
 setup(name='unetelan',
       version='0.1',
       description='Unet for IwM',
-      packages=find_packages()
+      packages=find_packages(), install_requires=['torchvision', 'numpy']
       )
--- unet/training/config.py
+++
@@ -1,9 +0,0 @@
-class Config:
-    bn = True
-    dp = 0.2
-    n_layers = 3
-    filters_base = 32
-    patch_border = 16
-    pad = 1
-    n_classes = 2
-    n_channels = 3
